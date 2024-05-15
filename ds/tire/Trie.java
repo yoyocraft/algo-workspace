2e75636c51f5f84b5b3e3003b90748926ba57a29
youyi <youyichannel@163.com>
2024-05-15 11:08:08 +0800

    add trie and prac1

--- ds/tire/Trie.java
+++ ds/tire/Trie.java
@@ -2,11 +2,26 @@ package ds.tire;
 
 public interface Trie {
 
+    /**
+     * 将指定的单词插入到前缀树中。
+     * 
+     */
     void insert(String word);
 
+    /**
+     * 如果之前word插入过前缀树，那么此时删掉一次，
+     * 如果之前word没有插入过前缀树，那么什么也不做
+     * 
+     */
     void erease(String word);
 
+    /**
+     * 查询前缀树里，word单词出现了几次
+     */
     int countWordsEqualTo(String word);
 
-    int countWordsStartingWith(String pre);
+    /**
+     * 查询前缀树里，有多少单词以pre做前缀
+     */
+    int countWordsStartWith(String pre);
 }
---
+++ ds/tire/TrieImpl2.java
@@ -0,0 +1,86 @@
+package ds.tire;
+
+import java.util.HashMap;
+
+public class TrieImpl2 implements Trie {
+
+    static class TrieNode {
+        public int pass;
+        public int end;
+        public HashMap<Integer, TrieNode> nexts;
+
+        public TrieNode() {
+            pass = 0;
+            end = 0;
+            nexts = new HashMap<>();
+        }
+    }
+
+    private TrieNode root;
+
+    public TrieImpl2() {
+        root = new TrieNode();
+    }
+
+    @Override
+    public void insert(String word) {
+        TrieNode node = root;
+        node.pass++;
+        for (int i = 0, path; i < word.length(); i++) {
+            path = word.charAt(i) - 'a'; // 对应走向哪条路
+            if (!node.nexts.containsKey(path)) {
+                node.nexts.put(path, new TrieNode());
+            }
+            node = node.nexts.get(path);
+            node.pass++;
+        }
+        node.end++;
+    }
+
+    @Override
+    public void erease(String word) {
+        if (countWordsEqualTo(word) <= 0) {
+            return;
+        }
+
+        TrieNode node = root, next;
+        node.pass--;
+        for (int i = 0, path; i < word.length(); i++) {
+            path = word.charAt(i) - 'a';
+            next = node.nexts.get(path);
+            if (--next.pass == 0) {
+                node.nexts.remove(path);
+                return;
+            }
+            node = next;
+        }
+        node.end--;
+    }
+
+    @Override
+    public int countWordsEqualTo(String word) {
+        TrieNode node = root;
+        for (int i = 0, path; i < word.length(); i++) {
+            path = word.charAt(i) - 'a';
+            if (!node.nexts.containsKey(path)) {
+                return 0;
+            }
+            node = node.nexts.get(path);
+        }
+        return node.end;
+    }
+
+    @Override
+    public int countWordsStartWith(String pre) {
+        TrieNode node = root;
+        for (int i = 0, path; i < pre.length(); i++) {
+            path = pre.charAt(i) - 'a';
+            if (!node.nexts.containsKey(path)) {
+                return 0;
+            }
+            node = node.nexts.get(path);
+        }
+        return node.pass;
+    }
+
+}
